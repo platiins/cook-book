3068f6e39afaaede5f4b7348235da6e0dc5710c2
platiins <valerijaaivanovaa@gmail.com>
2024-05-26 11:43:55 +0300

    adding structure, bootstrap and sass modules

---
+++ src/components/login.js
@@ -0,0 +1,43 @@
+import "../styles/styles.scss"; 
+
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+function Login() {
+  return (
+    <section className="login-container">
+      <h1 className="admin-title">ADMIN PANEL</h1>
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label>username</Form.Label>
+          <Form.Control
+            className="login-input"
+            name="username"
+            type="text"
+            id="username"
+            placeholder="enter username"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>password</Form.Label>
+          <Form.Control
+            className="login-input"
+            name="password"
+            type="password"
+            required
+            placeholder="enter password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="remember me" />
+        </Form.Group>
+        <Button variant="dark" type="submit">
+          login
+        </Button>
+      </Form>
+    </section>
+  );
+}
+
+export default Login;
