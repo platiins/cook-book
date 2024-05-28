fb0e023ff5b778c038236572ad6fdae97ef06814
platiins <valerijaaivanovaa@gmail.com>
2024-05-28 18:45:52 +0300

    fixed login page style

--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,10 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { useState } from "react";
-import "../styles/styles.scss";
 import { LuLogOut } from "react-icons/lu";
 import { FaCookieBite } from "react-icons/fa";
 import App from "../App";
+import "../styles/styles.scss";
 
 function Login() {
   const [username, setUsername] = useState("");
@@ -37,39 +37,41 @@ function Login() {
           </Button>
         </div>
       ) : (
-        <section className="login-container">
-          <h1 className="admin-title">COOK BOOK</h1>
-          <Form className="login-form" onSubmit={handleLogin}>
-            <Form.Group className="mb-3">
-              <Form.Label className="login-form__label">USERNAME:</Form.Label>
-              <Form.Control
-                className="login-form__input"
-                name="username"
-                type="text"
-                id="username"
-                value={username}
-                required
-                onChange={(event) => setUsername(event.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label className="login-form__label">PASSWORD:</Form.Label>
-              <Form.Control
-                className="login-form__input"
-                name="password"
-                type="password"
-                value={password}
-                required
-                onChange={(event) => setPassword(event.target.value)}
-              />
-            </Form.Group>
-            <Button className="login-btn" variant="dark" type="submit">
-              LOGIN
-              <FaCookieBite className="login-icon" />
-            </Button>
-            {error && <div>{error}</div>}
-          </Form>
-        </section>
+        <div className="login-page">
+          <section className="login-container">
+            <h1 className="admin-title">COOK BOOK</h1>
+            <Form className="login-form" onSubmit={handleLogin}>
+              <Form.Group className="mb-3">
+                <Form.Label className="login-form__label">USERNAME:</Form.Label>
+                <Form.Control
+                  className="login-form__input"
+                  name="username"
+                  type="text"
+                  id="username"
+                  value={username}
+                  required
+                  onChange={(event) => setUsername(event.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label className="login-form__label">PASSWORD:</Form.Label>
+                <Form.Control
+                  className="login-form__input"
+                  name="password"
+                  type="password"
+                  value={password}
+                  required
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+              </Form.Group>
+              <Button className="login-btn" variant="dark" type="submit">
+                LOGIN
+                <FaCookieBite className="login-icon" />
+              </Button>
+              {error && <div>{error}</div>}
+            </Form>
+          </section>
+        </div>
       )}
     </div>
   );
