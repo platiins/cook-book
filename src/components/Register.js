edb2f7555a45e951c4e57db3211497651da6b64c
platiins <valerijaaivanovaa@gmail.com>
2024-06-10 10:55:21 +0300

    adding validation to register form on register page

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,39 +1,141 @@
-import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import InputGroup from "react-bootstrap/InputGroup";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext } from "../context/theme";
 
 function Register() {
   const { theme } = useContext(ThemeContext);
+
+  const [registerData, setRegisterData] = useState({
+    firstName: "",
+    lastName: "",
+    username: "",
+    password: "",
+  });
+
+  const [firstNameError, setFirstNameError] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setRegisterData((prevData) => {
+      return {
+        ...prevData,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleBlur = (event) => {
+    const { name, value } = event.target;
+
+    switch (name) {
+      case "firstName":
+        if (value.length < 3) {
+          setFirstNameError("First Name must be at least 3 characters long");
+        } else {
+          setFirstNameError("");
+        }
+        break;
+      case "lastName":
+        if (value.length < 3) {
+          setLastNameError("Last Name must be at least 3 characters long");
+        } else {
+          setLastNameError("");
+        }
+        break;
+      case "username":
+        if (value.length < 3) {
+          setUsernameError("Username must be at least 3 characters long");
+        } else if (value.length > 10) {
+          setUsernameError("Username must be no more than 10 characters long");
+        } else {
+          setUsernameError("");
+        }
+        break;
+      case "password":
+        if (value.length < 6) {
+          setPasswordError("Password must be at least 6 characters long");
+        } else {
+          setPasswordError("");
+        }
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const handleSubmit = (event) => {gi
+    event.preventDefault();
+    event.stopPropagation();
+
+    setRegisterData({
+      firstName: "",
+      lastName: "",
+      username: "",
+      password: "",
+      email: "",
+    });
+
+    const form = event.target;
+    form.reset();
+  };
+
   return (
     <section
       className="register-page"
       style={{ backgroundColor: theme === "light" ? "#b4b093" : "#47482f" }}
     >
-      <Form className="register-page__form">
+      <Form
+        className="register-page__form"
+        onSubmit={handleSubmit}
+        method="get"
+      >
         <Row className="mb-3">
           <Col>
             <Form.Control
+              required
               placeholder="FIRST NAME"
+              value={registerData.firstName}
+              name="firstName"
               className="register-page__input"
               style={{
                 backgroundColor: theme === "light" ? "#47482f" : "#b4b093",
                 color: theme === "light" ? "#b4b093" : "#47482f",
               }}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              isInvalid={firstNameError !== ""}
             />
+            <Form.Control.Feedback type="invalid">
+              {firstNameError || "Please provide Full Name"}
+            </Form.Control.Feedback>
           </Col>
+
           <Col>
             <Form.Control
+              required
               placeholder="LAST NAME"
+              name="lastName"
+              value={registerData.lastName}
               className="register-page__input"
               style={{
                 backgroundColor: theme === "light" ? "#47482f" : "#b4b093",
                 color: theme === "light" ? "#b4b093" : "#47482f",
               }}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              isInvalid={lastNameError !== ""}
+              onSubmit={handleSubmit}
             />
+            <Form.Control.Feedback type="invalid">
+              {lastNameError || "Please provide a Last Name"}
+            </Form.Control.Feedback>
           </Col>
         </Row>
 
@@ -46,31 +148,50 @@ function Register() {
                   backgroundColor: theme === "light" ? "#47482f" : "#b4b093",
                   color: theme === "light" ? "#b4b093" : "#47482f",
                 }}
+                required
               >
                 @
               </InputGroup.Text>
               <Form.Control
+                required
                 id="inlineFormInputGroup"
+                name="username"
+                value={registerData.username}
                 placeholder="USERNAME"
-                className="register-page__input"
+                className="register-page__input username-input"
                 style={{
                   backgroundColor: theme === "light" ? "#47482f" : "#b4b093",
                   color: theme === "light" ? "#b4b093" : "#47482f",
                 }}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isInvalid={usernameError !== ""}
               />
+              <Form.Control.Feedback type="invalid">
+                {usernameError || "Please enter an username"}
+              </Form.Control.Feedback>
             </InputGroup>
           </Col>
           <Col>
             <Form.Group as={Col} controlId="formGridPassword">
               <Form.Control
+                required
+                value={registerData.password}
                 type="password"
+                name="password"
                 placeholder="PASSWORD"
                 className="register-page__input"
                 style={{
                   backgroundColor: theme === "light" ? "#47482f" : "#b4b093",
                   color: theme === "light" ? "#b4b093" : "#47482f",
                 }}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isInvalid={passwordError !== ""}
               />
+              <Form.Control.Feedback type="invalid">
+                {passwordError || "Please provide a password"}
+              </Form.Control.Feedback>
             </Form.Group>
           </Col>
         </Row>
@@ -78,6 +199,7 @@ function Register() {
         <Row className="mb-3 mt-3">
           <Form.Group as={Col} controlId="formGridEmail">
             <Form.Control
+              required
               type="email"
               placeholder="ENTER EMAIL"
               className="register-page__input"
@@ -142,7 +264,7 @@ function Register() {
           </Form.Group>
         </Row>
         <Row className="btn-row mt-4">
-          <Button
+          <button
             type="submit"
             className="register-page__btn register-page__input"
             style={{
@@ -151,7 +273,7 @@ function Register() {
             }}
           >
             REGISTER
-          </Button>
+          </button>
         </Row>
       </Form>
     </section>
